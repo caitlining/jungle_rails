e7c782a87e2bbc26b676ebcff94d0f42a4b3c754
caitlining <caitlincing@gmail.com>
2019-11-14 23:12:01 +0000

    Update login to ensure email is unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password_digest, presence: true
   
 end
